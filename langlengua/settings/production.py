46354f49dfacd9d399b49bc0269134fc98ded7a3
Projyot Srivastava <41471511+Projyots@users.noreply.github.com>
2020-05-09 03:16:15 +0530

    Update production.py

--- langlengua/settings/production.py
+++ langlengua/settings/production.py
@@ -57,7 +57,10 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SSL_REDIRECT", default=True)
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
+#ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
+
+ALLOWED_HOSTS = ["langlengua.herokuapp.com"]
+
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ("gunicorn", )
